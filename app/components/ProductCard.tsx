42f882405c15b2b2b8034a911bde88321d8e2205
wakanda8 <mohamadsafwan.1905@gmail.com>
2024-10-04 02:19:08 +0800

    [feat] new changes

---
+++ app/components/ProductCard.tsx
@@ -0,0 +1,10 @@
+"use client";
+import React from "react";
+
+const ProductCard = () => {
+  return (
+  <div><button onClick={() => console.log("Click")}>Add to Cart</button>
+  </div>)
+};
+
+export default ProductCard;
